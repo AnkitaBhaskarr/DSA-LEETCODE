c851ef366c59698635383c921669ebb488156f19
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-04-08 00:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 112.4 MB (60.06%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int total = 0;     // total gas - cost
+        int tank = 0;      // current tank
+        int start = 0;     // candidate start index
+
+        for (int i = 0; i < gas.size(); i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+
+            // If tank becomes negative, reset start
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        return (total >= 0) ? start : -1;
+    }
+};
