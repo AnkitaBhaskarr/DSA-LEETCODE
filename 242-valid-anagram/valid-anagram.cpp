7a8fb670813ce261a746c9e04ee9d95b4c7e51ca
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-06-08 19:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (25.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,24 @@
-#include <vector>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length())
+        if (s.length() != t.length()) {
             return false;
+        }
 
         vector<int> count(26, 0);
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
+        for (char c : s) {
+            count[c - 'a']++;
         }
 
-        for (int c : count) {
-            if (c != 0)
+        for (char c : t) {
+            count[c - 'a']--;
+        }
+
+        for (int x : count) {
+            if (x != 0) {
                 return false;
+            }
         }
 
         return true;
