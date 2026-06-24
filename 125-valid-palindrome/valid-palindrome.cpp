4002b2ad3aa2fffa0282bc94042f81642eb62888
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-06-24 23:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,54 @@
 class Solution {
+private:
+    bool valid(char ch) {
+        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            return 1;
+        }
+        return 0;
+    }
+    char toLowerCase(char ch) {
+        if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+            return ch;
+        else {
+            char temp = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+
+    bool checkPalindrome(string a) {
+        int s = 0;
+        int e = a.length()-1;
+
+        while(s<=e) {
+            if(a[s] != a[e])
+            {
+                return 0;
+            }
+            else {
+                s++;
+                e--;
+            }
+        }
+        return 1;
+    }
+
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.size() - 1;
 
-        while (left < right) {
-            while (left < right && !isalnum(s[left])) {
-                left++;
+        // faltu character htao
+        string temp = "";
+
+        for(int j=0; j<s.length(); j++) {
+            if(valid(s[j])) {
+                temp.push_back(s[j]);
             }
-
-            while (left < right && !isalnum(s[right])) {
-                right--;
-            }
-
-            if (tolower(s[left]) != tolower(s[right])) {
-                return false;
-            }
-
-            left++;
-            right--;
         }
 
-        return true;
+        // lowercase me krdo
+        for(int j=0; j<temp.length(); j++) {
+            temp[j] = toLowerCase(temp[j]);
+        }
+        // check palindrome
+        return checkPalindrome(temp);
     }
-};
+};    
