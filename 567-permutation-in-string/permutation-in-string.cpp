894d3d7ad00ee988ecddd7c482085eb4f6b5d65f
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-06-26 23:05:42 +0530

    Time: 2 ms (70.05%) | Memory: 10 MB (71.10%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,28 +1,55 @@
 class Solution {
+private:
+    bool checkEqual(int a[26], int b[26])
+    {
+        for(int i=0; i<26; i++) {
+            if(a[i] != b[i])
+                return 0;
+        }
+        return 1;
+    }    
 public:
     bool checkInclusion(string s1, string s2) {
-        int n = s1.size(), m = s2.size();
 
-        if (n > m) return false;
+        // character count array
+        int count1[26] = {0};
 
-        vector<int> cnt1(26, 0), cnt2(26, 0);
-
-        for (char c : s1)
-            cnt1[c - 'a']++;
-
-        for (int i = 0; i < n; i++)
-            cnt2[s2[i] - 'a']++;
-
-        if (cnt1 == cnt2) return true;
-
-        for (int i = n; i < m; i++) {
-            cnt2[s2[i] - 'a']++;         // add new character
-            cnt2[s2[i - n] - 'a']--;     // remove old character
-
-            if (cnt1 == cnt2)
-                return true;
+        for(int i=0; i<s1.length(); i++) {
+            int index = s1[i] - 'a';
+            count1[index]++;
         }
 
-        return false;
+        // traverse s2 string in window of size s1 length and compare
+
+        int i=0;
+        int windowSize = s1.length();
+        int count2[26] = {0};
+
+        // running for first window
+        while(i < windowSize && i<s2.length()) {
+            int index = s2[i] - 'a';
+            count2[index]++;
+            i++;
+        }
+
+        if( checkEqual(count1, count2) )
+           return 1;
+
+        // aage window process kro
+        while(i<s2.length()) {
+            char newChar = s2[i];
+            int index = newChar - 'a';
+            count2[index]++;
+
+            char oldChar = s2[i-windowSize];
+            index = oldChar - 'a';
+            count2[index]--;
+
+            i++;
+
+            if( checkEqual(count1, count2) )
+            return 1;
+        }
+        return 0;
     }
-};
+};    
