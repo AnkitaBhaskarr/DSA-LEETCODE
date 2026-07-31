3eb68e87cfb7362e8bc3436248d915f64000057e
AnkitaBhaskarr <ankitabhaskarr@gmail.com>
2026-08-01 00:30:24 +0530

    Time: 1 ms (54.88%) | Memory: 9.7 MB (76.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+
+        // If lengths are different, they can't be anagrams
         if (s.length() != t.length()) {
             return false;
         }
 
-        vector<int> count(26, 0);
+        // Create an array to count character frequencies
+        vector<int> charCounts(26, 0);
 
-        for (char c : s) {
-            count[c - 'a']++;
+        // Increment count for each character in 's'
+        // Decrement count for each character in 't'
+        for (int i = 0; i < s.length(); i++) {
+            charCounts[s[i] - 'a']++;
+            charCounts[t[i] - 'a']--;
         }
 
-        for (char c : t) {
-            count[c - 'a']--;
-        }
-
-        for (int x : count) {
-            if (x != 0) {
+        // Check if all counts are zero
+        for (int count : charCounts) {
+            if (count != 0) {
                 return false;
             }
         }
 
+        // All counts are zero, so 't' is an anagram of 's'
         return true;
     }
 };
